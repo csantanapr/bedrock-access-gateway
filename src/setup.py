7ed874dd1a49e9646ab4289d5e6c7b7ba37d0a1a
Carlos Santana <csantana23@gmail.com>
2024-06-19 12:10:45 -0400

    use pyproject
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="api",
-    version="0.1.0",
-    packages=setuptools.find_packages(where="api"),
-    package_dir={"": "api"},
-    python_requires=">=3.6",
-)
