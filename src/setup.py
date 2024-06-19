cb54131f52d3b454b670c958ec82505d5bdedec1
Carlos Santana <csantana23@gmail.com>
2024-06-19 11:50:57 -0400

    update setup
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="api",
     version="0.1.0",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="api"),
+    package_dir={"": "api"},
     python_requires=">=3.6",
 )
